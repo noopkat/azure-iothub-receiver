7c8e035159d826587d11a630248005d3522be546
Suz Hinton <suz.hinton@gmail.com>
2017-09-03 11:35:28 -0400

    minor spelling nits in pipe stream example

--- examples/pipe-example.js
+++ examples/pipe-example.js
@@ -6,7 +6,7 @@ const receiver = new Receiver({
   connectionString: process.env.IOT_CONN_STRING
 });
 
-//Example of using receiever stream throug piping to stdout
+//Example of using receiver stream through piping to stdout
 receiver.pipe(through.obj(function (chunk, enc, callback) {
   this.push(JSON.stringify(chunk.annotations));
 })).pipe(process.stdout);
